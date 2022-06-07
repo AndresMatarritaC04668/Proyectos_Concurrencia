7212bf758f5301c1c9ac1916dd7b483de6f5ce63
Esteban <esteban.moragarcia@ucr.ac.cr>
2022-06-06 20:47:34 -0600

    Documentacion Doxigen para GoldbachWebApp y SumGoldbachSolver

--- Proyecto1/src/webapp/GoldbachWebApp.hpp
+++ Proyecto1/src/webapp/GoldbachWebApp.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021 Jeisson Hidalgo-Cespedes. Universidad de Costa Rica. CC BY 4.0
+// Copyright Equipo Dinamita. Universidad de Costa Rica. CC BY 4.0
 
 #ifndef GOLDBACHWEBAPP_HPP
 #define GOLDBACHWEBAPP_HPP
@@ -10,7 +10,9 @@
 #include <regex>
 
 /**
-@brief A web application that calculates prime factors
+* @brief A web application that calculates
+* the Goldbach Conjecture
+*
 */
 class GoldbachWebApp : public HttpApp {
   /// Objects of this class cannot be copied
@@ -19,38 +21,136 @@ class GoldbachWebApp : public HttpApp {
  public:
  
   /// Constructor
+  /**
+   * @brief Constructor of class GoldbachWebApp
+   * 
+   */
   GoldbachWebApp();
+  
   /// Destructor
+  /**
+   * @brief Destructor of class GoldbachWebApp
+   * 
+   */
   ~GoldbachWebApp();
-  /// Called by the web server when the web server is started
+
+  /**
+   * @brief Called by the web server when the web server is started
+   * 
+   */
   void start() override;
-  /// Handle HTTP requests. @see HttpServer::handleHttpRequest()
-  /// @return true If this application handled the request, false otherwise
-  /// and another chained application should handle it
+
+  /**
+   * @brief chained application that should handle it
+   * 
+   * @param httpRequest has URL of the page asked
+   * @param httpResponse has the result of the page request
+   * @return true If this application handled the request, false otherwise
+   */
   bool handleHttpRequest(HttpRequest& httpRequest,
     HttpResponse& httpResponse) override;
-  std:: string mensaje(cola_t* cola);
-  /// Called when the web server stops, in order to allow the web application
-  /// clean up and finish as well
+
+  /**
+   * @brief add in a string the results of the goldbach conjectures
+   *  if the number has to show sums it adds the sums if not, only the
+   *  amount of sums
+   * 
+   * @param cola is the conteiner with the numbers
+   * @return string that has the final message for the user
+   */
+  std::string mensaje(cola_t* cola);
+
+  /**
+   * @brief Called when the web server stops, in order to allow the web application
+   * clean up and finish as well 
+   * 
+   */
   void stop() override;
 
 
  protected:
-  /// Handle HTTP requests. @see HttpServer::handleHttpRequest()
-  /// @return true If this application handled the request, false otherwise
-  /// Sends the homepage as HTTP response
+
+  /**
+   * @brief Sends the homepage as HTTP response 
+   * 
+   * @param httpRequest has URL of the page asked
+   * @param httpResponse has the result of the page request
+   * @return true If this application handled the request, false otherwise
+   */
   bool serveHomepage(HttpRequest& httpRequest, HttpResponse& httpResponse);
   /// Handle a HTTP request that starts with "/fact"
   /// @return true if the factorization was handled, false if it must be
   /// handled by another application
+
+  /**
+   * @brief Personal server of Goldbach Conjecture, it can calculate
+   * the conjecture to the numbers sent in the input
+   * 
+   * @param httpRequest 
+   * @param httpResponse 
+   * @return true if the conjecure was handled, false if it must be 
+   */
   bool serveGoldbach(HttpRequest& httpRequest, HttpResponse& httpResponse);
 
   /// Fix code redundancy in the following methods
+
+  /**
+   * @brief It implements the body html depends on the 
+   * option that was request
+   * 
+   * @param httpResponse is the http result that the user is waiting
+   * @param title message to show in html file
+   * @param cola is a conteiner with the numbers to work
+   * @param option is a flag, to know which html use
+   */
   void htmlResponse(HttpResponse& httpResponse, std::string title, cola_t* cola, int option);
+
+  /**
+   * @brief It has the begining of a html file and the end as well
+   * depends on the opcion it choose the begining or the end part
+   * 
+   * 
+   * @param httpResponse is the http result that the user is waiting
+   * @param title message to show in html file
+   * @param option is a flag, to know which html use
+   */
   void beginAndEndHtml(HttpResponse& httpResponse, std::string title, int option);
+
+  /**
+   * @brief Set the Header Response object
+   * 
+   * @param httpResponse is the http result that the user is waiting
+   */
   void setHeaderResponse(HttpResponse& httpResponse);
+
+  /**
+   * @brief Clean the URL, delete the garbage and let only 
+   * the useful parts to work 
+   * 
+   * @param httpRequest is the page asked
+   * @param inQuery use to identify the wep app
+   * @return string that it is the new URL 
+   */
   std::string decodeURI(HttpRequest& httpRequest, std::regex& inQuery);
+
+  /**
+   * @brief 
+   * 
+   * @param end this iterator has the value null, is the last one
+   * @param iter this iterator is the first position
+   * @param cola is a conteiner with the numbers to work
+   * @return int64_t the last number inserted in the conteiner
+   */
   int64_t storageData(std::sregex_iterator end, std::sregex_iterator iter, cola_t* cola);
+
+  /**
+   * @brief 
+   * 
+   * 
+   * @param resultado is the final message to show to the user
+   * @param nodo is the current working nodo
+   * @param i is the index use in nodo->desglose to know with what number work
+   */
   void addToResults(std::ostringstream& resultado, nodo_t* nodo, int& i);
 };
 
--- Proyecto1/src/webapp/SumGoldbachSolver.hpp
+++ Proyecto1/src/webapp/SumGoldbachSolver.hpp
@@ -8,9 +8,46 @@
 #include "cola.hpp"
 #include "SumGoldbachSolver.hpp"
 
+/**
+ * @brief This method is the general conjecture, is gets a 
+ * cola, then fill it with the numbers, calculate the conjecture 
+ * and the return it
+ * 
+ * @param cola is the conteiner to storege the data
+ * @return cola_t* with the Conjecture calculates in that cola 
+ */
 cola_t * goldBach(cola_t* cola);
-int conjeturaDebil(int64_t numero , nodo_t* , char);
-int conjeturaFuerte(int64_t numero , nodo_t*  , char);
+
+/**
+ * @brief If the number is odd, this is the method that
+ * calculate the conjecture
+ * it needs 3 primes numbers
+ * 
+ * @param numero ready to be calculated
+ * @param vector_nodo will storage the sums of the number
+ * @param signo flag if it has to show sums or not
+ * @return int the amount of sums
+ */
+int conjeturaDebil(int64_t numero , nodo_t* vector_nodo, char signo);
+
+/**
+ * @brief If the number is even, this is the method that
+ * calculate the conjecture
+ * it needs 2 primes numbers
+ * 
+ * @param numero ready to be calculated
+ * @param vector_nodo will storage the sums of the number
+ * @param signo flag if it has to show sums or not
+ * @return int the amount of sums
+ */
+int conjeturaFuerte(int64_t numero , nodo_t* vector_nodo, char signo);
+
+/**
+ * @brief Find out if the number is prime or not
+ * 
+ * @param numero number to be analized
+ * @return int 1 if it is prime, 0 if it is not prime
+ */
 int esPrimo(int64_t numero);
 
 #endif 
